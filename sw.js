1aea9f5e725e5ec44490dd0c76ddf227eec374ee
mitowav <adr1zmusiic@gmail.com>
2026-06-04 08:43:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,16 @@
-const CACHE = 'calc-v1';
+const CACHE = 'calc-v3';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => e.respondWith(
+  fetch(e.request).catch(() => caches.match(e.request))
+));
